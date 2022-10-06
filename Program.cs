2515bd383ebf85a1d27853059351f0d227d5a0f9
ryota46550 <108723344+ryota46550@users.noreply.github.com>
2022-10-06 15:17:35 +0900

    Revert "Age"
    This reverts commit d93a999f360b8f9c91f6ac67f9c32b56b66e919e.

--- Program.cs
+++ Program.cs
@@ -6,8 +6,7 @@ namespace Ex_2022_Template
         static void Main(string[] args)
         {
             var name = InputUtility.Input("Name:");
-            var age = InputUtility.InputNumber("Age:", 120);
-            Console.WriteLine($"Hello {name}! \n U are {age} yeaes old.");
+            Console.WriteLine($"Hello {name}!");
         }
     }
 }
